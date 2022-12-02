8978c56c7a9bf861d4e962511c2f02c777b62c4a
UP210356 <up210356@alumnos.upa.edu.mx>
2022-12-02 09:51:06 -0600

    PORTADA

---
+++ Front-end/Barco.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+void barco();
+int main(){
+barco();
+
+}
+
+void barco()
+{
+	cout<<"                              __    __    __  "<<endl;
+cout<<"                             |==|  |==|  |==| "<<endl;
+cout<<"                           __|__|__|__|__|__|_"<<endl;
+cout<<"                        __|___________________|___"<<endl;
+cout<<"                     __|__[]__[]__[]__[]__[]__[]__|___"<<endl;
+cout<<"                    |............................o.../"<<endl;
+cout<<"                     |.............................../"<<endl;
+cout<<"               _,~')_,~')_,~')_,~')_,~')_,~')_,~')/,~')_"<<endl;
+
+
+
+
+}
